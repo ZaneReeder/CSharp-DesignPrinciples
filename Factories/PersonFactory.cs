08ee8a7bb602e387abdea2f6ec5ebe25dbc952f5
Zane <zanereederx@gmail.com>
2018-07-16 18:22:45 -0500

    Factories Lecture Completed

---
+++ Factories/PersonFactory.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factories
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class PersonFactory
+    {
+        private int Id = 0;
+
+        public Person CreatePerson(string Name)
+        {
+            var person = new Person
+            {
+                Name = Name,
+                Id = Id
+            };
+            Id += 1;
+            return person;
+        }
+    }
+}
---
+++ Factories/Program.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factories
+{
+
+    public class Point
+    {
+        private double x, y;
+
+        // Without Factories using basic constructors
+        //public Point(double a, double b, 
+        //    CoordinateSystem system = CoordinateSystem.Cartesian)
+        //{
+        //    switch (system)
+        //    {
+        //        case CoordinateSystem.Cartesian:
+        //            x = a;
+        //            y = b;
+        //            break;
+        //        case CoordinateSystem.Polar:
+        //            x = a * Math.Cos(b);
+        //            y = a * Math.Sin(b);
+        //            break;
+        //        default:
+        //            throw new ArgumentOutOfRangeException(nameof(system), system, null);
+        //    }
+        //}
+
+        // Property
+        public static Point Origin => new Point(0, 0);
+
+        // static field initialized once. better
+        public static Point Origin2 = new Point(0, 0);
+
+        // Inner Factory
+        public static class Factory
+        {
+            public static Point NewCartesianPoint(double x, double y)
+            {
+                return new Point(x, y);
+            }
+
+            public static Point NewPolarPoint(double rho, double theta)
+            {
+                return new Point(rho * Math.Cos(theta), rho * Math.Sin(theta));
+            }
+        }
+
+        //Factory Method
+        //public static Point NewCartesianPoint(double x, double y)
+        //{
+        //    return new Point(x, y);
+        //}
+        
+        //public static Point NewPolarPoint(double rho, double theta)
+        //{
+        //    return new Point(rho * Math.Cos(theta), rho * Math.Sin(theta));
+        //}
+
+        private Point(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(x)}: {x}, {nameof(y)}: {y}";
+        }
+    }
+
+    /// Abstract Factory
+    
+    public interface IHotDrink
+    {
+        void Consume();
+    }
+
+    internal class Tea : IHotDrink
+    {
+        public void Consume()
+        {
+            Console.WriteLine("Tea consumed with milk.");
+        }
+    }
+
+    internal class Coffee : IHotDrink
+    {
+        public void Consume()
+        {
+            Console.WriteLine("Coffee is the greatest drink on the planet.");
+        }
+    }
+
+    public interface IHotDrinkFactory
+    {
+        IHotDrink Prepare(int amount);
+    }
+
+    internal class TeaFactory : IHotDrinkFactory
+    {
+        public IHotDrink Prepare(int amount)
+        {
+            Console.WriteLine($"Put in a tea bag, boil water, pour {amount} ml, add lemon.");
+            return new Tea();
+        }
+    }
+
+    internal class CoffeeFactory : IHotDrinkFactory
+    {
+        public IHotDrink Prepare(int amount)
+        {
+            Console.WriteLine($"Grind coffee beans, boil water, pour {amount} ml.");
+            return new Coffee();
+        }
+    }
+
+    public class HotDrinkMachine
+    {
+        // Breaks Open Close Principle.
+        //public enum AvailableDrink
+        //{
+        //    Coffee, Tea
+        //}
+
+        //private Dictionary<AvailableDrink, IHotDrinkFactory> factories =
+        //    new Dictionary<AvailableDrink, IHotDrinkFactory>();
+
+        //public HotDrinkMachine()
+        //{
+        //    foreach(AvailableDrink drink in Enum.GetValues(typeof(AvailableDrink)))
+        //    {
+        //        var factory = (IHotDrinkFactory)Activator.CreateInstance(
+        //            Type.GetType("Factories." + Enum.GetName(typeof(AvailableDrink), drink) + "Factory")
+        //            );
+        //        factories.Add(drink, factory);
+        //    }
+        //}
+
+        //public IHotDrink MakeDrink(AvailableDrink drink, int amount)
+        //{
+        //    return factories[drink].Prepare(amount);
+        //}
+
+
+        // Use reflection
+        private List<Tuple<string, IHotDrinkFactory>> factories = 
+            new List<Tuple<string, IHotDrinkFactory>>();
+
+        public HotDrinkMachine()
+        {
+            foreach (var t in typeof(HotDrinkMachine).Assembly.GetTypes())
+            {
+                if (typeof(IHotDrinkFactory).IsAssignableFrom(t) && !t.IsInterface)
+                {
+                    factories.Add(Tuple.Create(
+                        t.Name.Replace("Factory", string.Empty),
+                        (IHotDrinkFactory)Activator.CreateInstance(t)
+                        ));
+                }
+            }
+        }
+
+        public IHotDrink MakeDrink()
+        {
+            Console.WriteLine("Available Drinks: ");
+            for (var index = 0; index < factories.Count; index++)
+            {
+                var tuple = factories[index];
+                Console.WriteLine($"{index}: {tuple.Item1}");
+            }
+
+            while (true)
+            {
+                string s;
+                if ((s = Console.ReadLine()) != null
+                    && int.TryParse(s, out int i)
+                    && i>=0
+                    && i < factories.Count)
+                {
+                    Console.WriteLine("Specify amount: ");
+                    s = Console.ReadLine();
+                    if (s != null
+                        && int.TryParse(s, out int amount)
+                        && amount > 0)
+                    {
+                        return factories[i].Item2.Prepare(amount);
+                    }
+                }
+                Console.WriteLine("Incorrect input, try again!");
+            }
+        }
+    }
+
+
+    class Program
+    {
+        /// <summary>
+        /// Motivation
+        ///     Object Creation logic is too convoluted
+        ///     Constructor is not descriptive
+        ///         Name mandated by name of containing type
+        ///         Cannnot overload constructor
+        ///         Optional parameter hell
+        ///     Object creation is non-piecwise
+        ///         Outsourced to 
+        ///             a seperate function (Factory Method)
+        ///             Exist in seperate class (Factory)
+        ///             Create hierarchy of factories with Abstract Factory
+        /// </summary>
+        
+        static void Main(string[] args)
+        {
+
+            var machine = new HotDrinkMachine();
+            var drink = machine.MakeDrink();
+            drink.Consume();
+        }
+    }
+}
